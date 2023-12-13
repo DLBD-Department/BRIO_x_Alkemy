325d8b602d4782895cf67d108d5e1a72d1d37ae4
giannu <90277177+giannu@users.noreply.github.com>
2023-12-13 10:05:32 +0100

    forgot to push some modified functions

--- src/utils/funcs.py
+++ src/utils/funcs.py
@@ -17,7 +17,10 @@ def handle_multiupload(req: request, label: str, path: str) -> None:
 
 def handle_ref_distributions(rootvar: str, targetvar: str, df : pd.DataFrame, dict_vars: dict) -> list:
     nroot = len(df[rootvar].unique())
-    ntarget = len(df[targetvar].unique())
+    if dict_vars['target_type']=='probability':
+        ntarget=dict_vars['nbins']
+    else:
+        ntarget = len(df[targetvar].unique())
     final_list = []
     intermediate_list = []
     for i in range(nroot):
@@ -43,9 +46,41 @@ def order_violations(viol: dict) -> dict:
     sorted_dict = dict(sorted_entries_with_middle_values + [(key, value) for key, value in viol.items() if get_middle_value(value) is None])
     return sorted_dict
 
-def write_reference_distributions_html(rootvar: str, targetvar: str, df: pd.DataFrame) -> str:
+def write_reference_distributions_html_old(rootvar: str, targetvar: str, df: pd.DataFrame) -> str:
     nroot = len(df[rootvar].unique())
     ntarget = len(df[targetvar].unique())
+    tot_refs = nroot * ntarget
+    begin = '<div class="row" id="ref_dist">'
+    end = '</div>'
+    tot_html = ""
+    nrows = tot_refs // 4
+    if nrows == 0:
+        nrows = 1
+    ncols = tot_refs % 4
+    c = 0
+    d = 0
+    for n in range(nrows):
+        tot_html += f'<div class="row" id="ref_dist{n}">'
+        for j in range(tot_refs):
+            if j != 0 and j % 4 == 0:
+                break
+            tot_refs -= 1
+            tot_html += '<div class="col-3">'
+            tot_html += f'<input type="number" class="form-control" placeholder="{rootvar}_{c}_{targetvar}_{d}_ref" name="prob_{c}_{d}" id="prob_{c}_{d}" min="0" max="1" step=".01">'
+            d += 1
+            if d == ntarget:
+                d = 0
+                c += 1
+            tot_html += '</div>'
+        tot_html += '</div><br>'
+    return tot_html
+
+def write_reference_distributions_html(rootvar: str, targetvar: str, df: pd.DataFrame, target_type:str, n_bins:int) -> str:
+    nroot = len(df[rootvar].unique())
+    if target_type=='probability':
+        ntarget = n_bins
+    else:
+        ntarget = len(df[targetvar].unique())
     tot_refs = nroot * ntarget
     begin = '<div class="row" id="ref_dist">'
     end = '</div>'
