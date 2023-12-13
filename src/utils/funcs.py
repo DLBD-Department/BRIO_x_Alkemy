8a7c4707719f3ec72845935547396f779f971d87
ChristianQuaggioAlkemy <christian.quaggio@alkemy.com>
2023-12-13 10:35:35 +0100

    adjusted number of cells in freqvsref

--- src/utils/funcs.py
+++ src/utils/funcs.py
@@ -3,10 +3,12 @@ import pandas as pd
 import numpy as np
 import os
 
+
 def allowed_file(filename: str) -> str:
     ALLOWED_EXTENSIONS = {'pkl', 'csv', 'ipynb', 'py'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def handle_multiupload(req: request, label: str, path: str) -> None:
     files_list = req.files.getlist(label)
@@ -15,10 +17,11 @@ def handle_multiupload(req: request, label: str, path: str) -> None:
         save_path = os.path.join(path, name)
         file.save(save_path)
 
-def handle_ref_distributions(rootvar: str, targetvar: str, df : pd.DataFrame, dict_vars: dict) -> list:
+
+def handle_ref_distributions(rootvar: str, targetvar: str, df: pd.DataFrame, dict_vars: dict) -> list:
     nroot = len(df[rootvar].unique())
-    if dict_vars['target_type']=='probability':
-        ntarget=dict_vars['nbins']
+    if dict_vars['target_type'] == 'probability':
+        ntarget = dict_vars['nbins']
     else:
         ntarget = len(df[targetvar].unique())
     final_list = []
@@ -30,6 +33,7 @@ def handle_ref_distributions(rootvar: str, targetvar: str, df : pd.DataFrame, di
         intermediate_list = []
     return final_list
 
+
 def order_violations(viol: dict) -> dict:
     # Function to get the middle value from the tuple
     def get_middle_value(item):
@@ -40,55 +44,25 @@ def order_violations(viol: dict) -> dict:
 
     # Sort entries with valid middle values
     sorted_entries_with_middle_values = sorted(((key, value) for key, value in viol.items() if get_middle_value(value) is not None),
-                                            key=lambda x: get_middle_value(x[1]), reverse=True)
+                                               key=lambda x: get_middle_value(x[1]), reverse=True)
 
     # Sort entries with None middle values and append them at the end
-    sorted_dict = dict(sorted_entries_with_middle_values + [(key, value) for key, value in viol.items() if get_middle_value(value) is None])
+    sorted_dict = dict(sorted_entries_with_middle_values + [(key, value)
+                       for key, value in viol.items() if get_middle_value(value) is None])
     return sorted_dict
 
-def write_reference_distributions_html_old(rootvar: str, targetvar: str, df: pd.DataFrame) -> str:
-    nroot = len(df[rootvar].unique())
-    ntarget = len(df[targetvar].unique())
-    tot_refs = nroot * ntarget
-    begin = '<div class="row" id="ref_dist">'
-    end = '</div>'
-    tot_html = ""
-    nrows = tot_refs // 4
-    if nrows == 0:
-        nrows = 1
-    ncols = tot_refs % 4
-    c = 0
-    d = 0
-    for n in range(nrows):
-        tot_html += f'<div class="row" id="ref_dist{n}">'
-        for j in range(tot_refs):
-            if j != 0 and j % 4 == 0:
-                break
-            tot_refs -= 1
-            tot_html += '<div class="col-3">'
-            tot_html += f'<input type="number" class="form-control" placeholder="{rootvar}_{c}_{targetvar}_{d}_ref" name="prob_{c}_{d}" id="prob_{c}_{d}" min="0" max="1" step=".01">'
-            d += 1
-            if d == ntarget:
-                d = 0
-                c += 1
-            tot_html += '</div>'
-        tot_html += '</div><br>'
-    return tot_html
 
-def write_reference_distributions_html(rootvar: str, targetvar: str, df: pd.DataFrame, target_type:str, n_bins:int) -> str:
+def write_reference_distributions_html(rootvar: str, targetvar: str, df: pd.DataFrame, target_type: str, n_bins: int) -> str:
     nroot = len(df[rootvar].unique())
-    if target_type=='probability':
+    if target_type == 'probability':
         ntarget = n_bins
     else:
         ntarget = len(df[targetvar].unique())
     tot_refs = nroot * ntarget
-    begin = '<div class="row" id="ref_dist">'
-    end = '</div>'
     tot_html = ""
     nrows = tot_refs // 4
-    if nrows == 0:
-        nrows = 1
-    ncols = tot_refs % 4
+    if nrows % 4 != 0:
+        nrows += 1
     c = 0
     d = 0
     for n in range(nrows):
